4d3f541649a5e1a309e0f5bd546d58b7306f319c
Andjelka Lukovic <andjelkal@live.com>
2020-02-05 09:58:19 +0100

    added Nedelja2

---
+++ Nedelja2/zadatak2.js
@@ -0,0 +1,8 @@
+let cena=50
+let novac=150
+
+if (cena>novac) {
+    console.log ('Nemate dovoljno novca ' + cena);
+}
+else {console.log 
+    ('Uspesno ste kupili proizvod ' + ( novac-cena))};
---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,5 @@
+for (i = 5; ; i++) {
+    if (i > 1000 * 5) { break; }
+    if (i % 5 == 0)
+        console.log(i);
+}
