0064d0ed989809d6d15f6b8318f89174b648023b
Andjelka Lukovic <andjelkal@live.com>
2020-02-07 16:01:30 +0100

    changed zadatak9.js

--- Nedelja2/zadatak9.js
+++ Nedelja2/zadatak9.js
@@ -1,5 +1,4 @@
 let n=5, znak='#';
 for (i=1; i<=n; i++){
-let red=' '.repeat(n-i)+znak.repeat(i)
-+' '+znak.repeat(i)+' '.repeat(n-i);
+let red=' '.repeat(n-i)+znak.repeat(i)+' '+znak.repeat(i)+' '.repeat(n-i);
 console.log(red);}
