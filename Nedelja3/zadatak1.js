839cad665bc419ec9b2268d55ecd0fb5ce42fadd
Andjelka Lukovic <andjelkal@live.com>
2020-02-12 11:09:27 +0100

    added Nedelja3

---
+++ Nedelja3/zadatak1.js
@@ -0,0 +1,4 @@
+function broj (){
+    let br=8, br=62, br=14;
+    return (Math.max (br));
+}
---
+++ Nedelja3/zadatak2.js
@@ -0,0 +1,5 @@
+function pizza (){
+    let r=10, cena=500;
+    let P=Math.PI*r**2;
+    return cena/P;
+}
