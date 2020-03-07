941bb1ccfe3bd664b318ffa1776819f79b5ccdae
andjelkalukovic <60389145+andjelkalukovic@users.noreply.github.com>
2020-03-07 22:52:06 +0100

    Add files via upload

--- Nedelja6/src/index.js
+++ Nedelja6/src/index.js
@@ -1,39 +1,46 @@
-import {LaunchList, InfoHed} from "./components/launch_list";
-import optionRakete from "./components/select";
+import { LaunchList, InfoHed, LaunchListDates } from "./components/launch_list";
 
-const app=document.querySelector('#app');
+const app = document.querySelector('#app');
+//const divSelekt=document.querySelector('#godine');
+//const lista=document.querySelector('#ispis');
 
 // const header=Header()
-const header=InfoHed();
-const launch_list=LaunchList();
+const header = InfoHed();
+const launch_list = LaunchList();
+const filters = document.createElement('section');
+const selektor = document.querySelector('#selektor');
 
-const filters=document.createElement('section');
-const select2=optionRakete();
+for (let i = 1999; i < 2030; i++) {
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    selektor.appendChild(option);
+}
 
-/*const selectGodine=document.querySelector('#selektorid');
-let ispis=document.createElement('div');
-        ispis.id = 'ispis';
-        //app.appendChild(ispis);
-selectGodine.addEventListener('change', () => {
-    const godina=document.querySelectorAll('#godina');
-    
-    godina.forEach(year => {
-       year.parentElement.parentElement.style.display = 'none';
-        if (selectGodine.value == 1) {
-        } else if (selectGodine.value == year.innerHTML) {
-           ;
-        } else if  (selectGodine.value == 2011) {
-            ispis.innerHTML = `Ove godine nije bilo lansiranja rakete.`;
+selektor.addEventListener('change', () => {
+    let kojalista = selektor.value;
+    if (kojalista == 'SVE') {
+        if (app.contains(document.querySelector('.launch-dates'))) {
+            app.removeChild(document.querySelector('.launch-dates'));
+
         }
-    })
-})   */
+        app.appendChild(launch_list);
+    } else {
+        let launch_dates = LaunchListDates(kojalista);
+        console.log(launch_list);
+        if (app.contains(document.querySelector('.launch-list'))) {
+            app.removeChild(launch_list);
+        }
+        if (app.contains(document.querySelector('.launch-dates'))) {
+            app.removeChild(document.querySelector('.launch-dates'));
+        }
+        app.appendChild(launch_dates);
+    }
+})
 
-const footer=document.createElement('footer');
-footer.innerText='Developed by ITBootCamp';
-
+const footer = document.querySelector('#footer');
+footer.innerText = 'Developed by ITBootCamp';
 
 app.appendChild(header);
 app.appendChild(launch_list);
-app.appendChild(footer);
 app.appendChild(filters);
-app.appendChild(select2);
